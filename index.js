88d4406e10a985f4440c20cf7e7160bdf4d47686
Yurina Shin <yurinas@gmail.com>
2020-06-07 21:28:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function produceDrivingRange(blockRange) {
   return function(one, two){
-    return Math.abs(Integer.parseInt(one.slice(0,2)) - Integer.parseInt(one.slice(0,2));
+    let oneNum = Integer.parseInt(one.slice(0,2))
+    let twoNum = Integer.parseInt(two.slice(0,2))
+    let difference = Math.abs(oneNum - twoNum)
+
+    if (difference < blockRange) {
+      return `within range by ${blockRange}`;
+    } else {
+      return `${difference} blocks out of range`;
+    }
   }
 }
